3e6a15b4c2b053a6e13567436cdf2f56fc378376
ohserin <dhtpfls321@gmail.com>
2024-11-11 16:45:41 +0900

    Update Unit getter and setter

--- src/zombie/Unit.java
+++ src/zombie/Unit.java
@@ -1,15 +1,37 @@
 package zombie;
 
+import java.util.Random;
+
 public class Unit {
 	protected final int MAX_HP;
 	protected int hp;
 	protected int pos;
-	protected String name;
+	protected Random ran;
 
-	protected Unit(int hp, int pos, String name) {
+	protected Unit(int hp, int pos) {
 		MAX_HP = hp;
 		this.hp = hp;
 		this.pos = pos;
-		this.name = name;
+		this.ran = new Random();
+	}
+
+	public int getPos() {
+		return this.pos;
+	}
+
+	public void setPos(int pos) {
+		this.pos = pos;
+	}
+
+	public int getHp() {
+		return this.hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getMAX_HP() {
+		return this.MAX_HP;
 	}
 }
