3320e06bb4902f9a7b5bce2485255596ea4ce2f0
ohserin <dhtpfls321@gmail.com>
2024-11-12 11:18:19 +0900

    Update Zombie class method : attack()

--- src/zombie/Zombie.java
+++ src/zombie/Zombie.java
@@ -7,12 +7,18 @@ abstract public class Zombie extends Unit implements Attackable {
 	}
 
 	public void attack(Unit hero) {
-		attack = ran.nextInt(attack) + 1;
+		attack = ran.nextInt(4) + 1;
 
 		hero.setHp(hero.getHp() - attack);
 		if (hero.getHp() <= 0) {
 			hero.setHp(0);
 		}
+
+		this.setHp(this.getHp() + attack / 2);
+
+		System.out.println(
+				"좀비가 " + attack + "의 공격력으로 공격 :" + " 현재 Hero hp : " + hero.getHp() + ",좀비 체력 회복 " + this.getHp());
+
 	}
 
 }
