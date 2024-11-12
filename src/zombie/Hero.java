c9c791375a5008c985e472f9128a60ded4069b29
ohserin <dhtpfls321@gmail.com>
2024-11-12 10:39:15 +0900

    Update Hero class method : attack()

--- src/zombie/Hero.java
+++ src/zombie/Hero.java
@@ -1,24 +1,51 @@
 package zombie;
 
 public class Hero extends Unit implements Attackable, Recoverable {
-	protected Hero(int hp, int pos, int attack) {
-		super(hp, pos, attack);
-	}
-
 	private int count;
 
-	@Override
-	public void attack(Unit unit) {
-		if (unit instanceof Unit) {
-			Unit target = (Unit) unit;
-		}
+	protected Hero(int hp, int pos, int attack) {
+		super(hp, pos, attack);
+		this.count = 4;
+	}
 
+	@Override
+	public void attack(Unit enemy) {
+
+		if (enemy instanceof Boss) {
+			Boss boss = (Boss) enemy;
+			attack = ran.nextInt(10) + 1;
+			if (boss.getShield() > 0) {
+				int r = boss.getShield() - attack;
+				if (r >= 0) {
+					boss.setShield(boss.getShield() - attack);
+				} else {
+					boss.setShield(0);
+					boss.setHp(boss.getHp() - r);
+				}
+			} else {
+				boss.setHp(boss.getHp() - attack);
+			}
+
+			if (boss.getHp() <= 0) {
+				boss.setHp(0);
+			}
+			System.out.println(
+					"히어로가 " + attack + "의 공격력으로 공격 :" + " 현재 Boss hp : " + boss.getHp() + "현재 Boss Shield : " + boss.getShield());
+		} else {
+
+			attack = ran.nextInt(10) + 1;
+			enemy.setHp(enemy.getHp() - attack);
+			if (enemy.getHp() <= 0) {
+				enemy.setHp(0);
+			}
+			System.out.println("히어로가 " + attack + "의 공격력으로 공격 :" + " 현재 Zombie hp : " + enemy.getHp());
+		}
 	}
 
 	public void recovery() {
 		if (count > 0) {
 			setHp(getHp() + 200);
-			System.out.println("체력 회복해서 " + getHp() + "가 되었습니다."); // 회복된 체력 출력
+			System.out.println("체력 회복해서 " + getHp() + "가 되었습니다.");
 			count -= 1;
 		} else {
 			System.out.println("모두 사용했습니다.");
